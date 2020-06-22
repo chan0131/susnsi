09ce7f30226617ab7707047fd09e45e6579e890b
chan0131 <67203613+chan0131@users.noreply.github.com>
2020-06-22 14:23:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -102,6 +102,9 @@ client.on('message', (message) => {
   if (message.content === '수환의띠띠'){
     message.reply('의띠띠');
   }
+  if (message.content === '기가'){
+    message.reply('의띠띠');
+  }
 });
 
 
